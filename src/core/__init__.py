151e757f78a7a15a5d6954ac8c16d2afc7cd0931
tsamaan <tsamaanpiroli@uade.edu.ar>
2026-04-06 16:39:40 -0300

    Reset main

--- src/core/__init__.py
+++
@@ -1,3 +0,0 @@
-from .tour_orchestrator import TourContext, TourOrchestrator, TourPlan
-
-__all__ = ["TourContext", "TourOrchestrator", "TourPlan"]
--- src/vision/__init__.py
+++
@@ -1,8 +0,0 @@
-from .vision_processor import CameraModel, OdometryVector, PoseEstimate, VisionProcessor
-
-__all__ = [
-    "CameraModel",
-    "OdometryVector",
-    "PoseEstimate",
-    "VisionProcessor",
-]
