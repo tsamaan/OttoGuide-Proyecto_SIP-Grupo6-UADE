5e025a77debc7825f78a501075a0d8edf7985aa7
LucasCap12 <LucasCap12@users.noreply.github.com>
2026-05-07 14:13:42 -0300

    feat(sim): habilitar Foxy/Humble en launcher SITL tmux

--- codigo ottoguide/scripts/launch_sitl_tmux.sh
+++ codigo ottoguide/scripts/launch_sitl_tmux.sh
@@ -24,7 +24,7 @@ tmux split-window -v -t "${SESSION_NAME}:0.0"
 tmux split-window -v -t "${SESSION_NAME}:0.1"
 
 tmux send-keys -t "${SESSION_NAME}:0.0" "cd '${MUJOCO_DIR}' && python3 simulate.py g1/scene_29dof.xml" C-m
-tmux send-keys -t "${SESSION_NAME}:0.1" "source /opt/ros/humble/setup.bash && cd '${ROS2_DIR}' && ros2 launch unitree_ros2 g1_sitl_bridge.launch.py" C-m
+tmux send-keys -t "${SESSION_NAME}:0.1" "if [ -f /opt/ros/foxy/setup.bash ]; then source /opt/ros/foxy/setup.bash; else source /opt/ros/humble/setup.bash; fi && cd '${ROS2_DIR}' && ros2 launch unitree_ros2 g1_sitl_bridge.launch.py" C-m
 tmux send-keys -t "${SESSION_NAME}:0.2" "cd '${ISAAC_DIR}' && python3 sim_main.py --usd '${ROOT_DIR}/libs/unitree_sim_isaaclab-main/robots/g1/g1.usd'" C-m
 tmux send-keys -t "${SESSION_NAME}:0.3" "cd '${ROOT_DIR}' && export ROBOT_MODE=sim && uvicorn main:app --host 0.0.0.0 --port 8000" C-m
 
